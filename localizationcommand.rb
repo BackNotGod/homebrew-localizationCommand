d7e973b0187ec7ddd74b17269e47ba6389fed254
BackNotGod <754748816@qq.com>
2017-03-20 16:58:32 +0800

    Update localizationcommand.rb

--- localizationcommand.rb
+++ localizationcommand.rb
@@ -5,7 +5,7 @@
 class Localizationcommand < Formula
   desc "Command Line for iOS app of localization(support '*.m','*.swift')"
   homepage "https://github.com/BackNotGod/localizationCommand"
-  url "https://github.com/BackNotGod/localizationCommand/archive/0.0.1.tar.gz"
+  url "https://github.com/BackNotGod/localizationCommand/archive/0.0.2.tar.gz"
   sha256 "ae296781e372a85ea637c5cfc1cd5ef4602f397ca24a572ab6d41a2205bfae8c"
 
   depends_on :xcode => ["8.0", :build]
